fcc2844b4be3b847c5aa01658cd6243ffc97c3c8
Michael <michaelmccabe711@gmail.com>
2019-03-31 16:37:18 -0400

    first commit

---
+++ server/routes/main-app/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const youtubeScraper = require('../../utils')
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  // page stays responsive for longer than 2 minutes
+  req.connection.setTimeout(Number.MAX_SAFE_INTEGER);
+  (async function oneVideo(req,res) {
+    const youtubeLink = req.body.youtubeLink
+    const data = await youtubeScraper.oneVideo(req,res,youtubeLink)
+    console.log(data)
+    res.render('home', {
+      retrievedData: data,
+    })
+  })(req,res)
+})
+
+router.get('/', (req, res) => {
+  res.render('home');
+});
+
+module.exports = router;
