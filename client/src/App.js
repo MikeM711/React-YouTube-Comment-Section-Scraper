1bac1902d9c898f325e52e664d640424cdb2db89
Michael <michaelmccabe711@gmail.com>
2019-04-02 16:05:46 -0400

    created a form, listened to changes on that form and updated the state; when onSubmit is fired, we used that state data as part of a POST request to the backend, backend receives the request and 1) sends a response back to the frontend and 2) declares an end to the connection (all by using res.send), axios receives the backend data response and the promise is completed because the connection ended, using the successful then(), we set the state with the new response data, the new state is rendered into JSX and shown in browser

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 
 class App extends Component {
   state = {
-    data: ''
+    data: '',
+    post:'',
+    response: '',
   }
   componentDidMount() {
     axios.get('/api/hello')
@@ -15,14 +17,49 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
+  // Listens to every change of value in input field
+  handleChange = (e) => {
+    this.setState({
+      post: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('form submitted', this.state.post)
+    //2nd param passes data, the data is attached to the 'post' param - req.body.post
+    axios.post('/api/world',{
+      post: this.state.post
+    })
+      .then(res => {
+        console.log(res)
+        // put the res.data inside our state, so we can use it in the component
+        this.setState({
+          post: '',
+          response: res.data
+        });
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
     const { data } = this.state
     return (
       <div className="app">
         <div>
-        <h2>Hello world</h2>
-        <p>My React Application</p>
-        <p>Express data: {data}</p>
+          <h2>Hello world</h2>
+          <p>My React Application</p>
+          <p>Express data: {data}</p>
+        </div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            {/* 'value' property used to make value = '' after submit, that's it */}
+            <input type="text" onChange={this.handleChange} value={this.state.post} />
+            <button>Submit</button>
+          </form>
+        </div>
+        <div>
+          <p>The response to your form was: {this.state.response}</p>
         </div>
       </div>
     )
--- server.js
+++ server.js
@@ -25,12 +25,13 @@ app.get('/api/hello', (req,res) => {
   res.send({ express: 'Hello From Express' })
 })
 
-// app.post('/api/world', (req, res) => {
-//   console.log(req.body);
-//   res.send(
-//     `I received your POST request. This is what you sent me: ${req.body.post}`,
-//   );
-// });
+// POST method route example
+app.post('/api/world', (req, res) => {
+  console.log(req.body.post);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
 
 const PORT = process.env.PORT || 5000;
 
