ec2ed1ea107b598db740e8ab90be772239e1a9f5
Michael <michaelmccabe711@gmail.com>
2019-04-04 18:30:12 -0400

    socketio connection test

--- server.js
+++ server.js
@@ -3,50 +3,77 @@ const exphbrs = require('express-handlebars')
 const routes = require('./server/routes')
 const bodyParser = require('body-parser');
 
+const http = require("http");
+const socketIo = require("socket.io");
+
 const app = express();
 
 // body parser
 app.use(bodyParser.urlencoded({
-  extended: false
+extended: false
 }));
 app.use(bodyParser.json());
 
 // routes
 app.use('/',routes)
 
+// Server stuff
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", socket => {
+ console.log('a user connected');
+ // console.log(socket.id)
+ socket.emit('FromAPI', 'instant msg');
+});
+
+app.set('socketio', io);
+
+// Testing our socketIO - Frontend will send a GET request to this route when mounted
+app.get('/timer', (req, res) => {
+ console.log('hit')
+ res.send('my timer')
+
+ const io = req.app.get('socketio');
+ io.emit('FromAPI', 'message from /timer');
+})
+
 // Handlebars
 app.set('views','./server/views')
 app.engine('.hbs',exphbrs({
-  extname: '.hbs'
+extname: '.hbs'
 }))
 app.set('view engine', '.hbs');
 
 app.get('/api/hello', (req,res) => {
-  res.send({ express: 'Hello From Express' })
+res.send({ express: 'Hello From Express' })
 })
 
 // POST method route example
 app.post('/api/world', (req, res) => {
-  console.log(req.body.post);
-  res.write('1. hello world ')
-  setTimeout(() => {
-    res.write(
-      `2. I received your POST request. This is what you sent me: ${req.body.post}`,
-    );
-  },3*1000)
+ console.log(req.body.post);
+ res.write('1. hello world ')
+ io.emit('FromAPI', 'starting /api/world');
+ setTimeout(() => {
+   res.write(
+     `2. I received your POST request. This is what you sent me: ${req.body.post}`,
+   );
+   io.emit('FromAPI', '3 seconds');
+ },3*1000)
 
-  setTimeout(() => {
-    res.end();
-  },10*1000)
+ setTimeout(() => {
+    io.emit('FromAPI', '10 seconds');
+   res.end();
+ },10*1000)
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, (err) => {
-  if(!err){
-    console.log('site is live')
-  }
-  else {
-    console.log(err)
-  }
+server.listen(PORT, (err) => {
+if(!err){
+  console.log('site is live')
+}
+else {
+  console.log(err)
+}
 })
