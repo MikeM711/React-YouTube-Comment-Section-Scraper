9a3723b5ce260be2f7187b5eb5e8f312be0301ea
Michael <michaelmccabe711@gmail.com>
2019-04-05 14:19:30 -0400

    getting heroku production to work 1

--- server.js
+++ server.js
@@ -73,6 +73,17 @@ app.post('/ytdata', (req,res) => {
 //  },10*1000)
 // });
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, (err) => {
