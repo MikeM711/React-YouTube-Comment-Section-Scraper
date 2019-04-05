dd84630c9d5909d30aa3f76d761c93d0e305c4a8
Michael <michaelmccabe711@gmail.com>
2019-04-04 22:04:43 -0400

    IO responses created to indicate progress to the user, and splitting client into components

--- server/routes/main-app/index.js
+++ server/routes/main-app/index.js
@@ -9,12 +9,12 @@ router.post('/youtubeData', (req, res) => {
   (async function oneVideo(req,res) {
     const youtubeLink = req.body.url
     const io = req.app.get('socketio');
-    io.emit('FromAPI', 'message from /youtubeData');
-    const data = await youtubeScraper.oneVideo(req,res,youtubeLink)
+    io.emit('ScrollData', 'Starting Scroll');
+    const data = await youtubeScraper.oneVideo(req,res,youtubeLink,io)
     console.log(data)
     // as long as we show the bottom line, React can get the data
     // res.write(data) - this will show up in the frontend
-    io.emit('FromAPI', data);
+    io.emit('ResultData', data);
 
     res.end()
 
