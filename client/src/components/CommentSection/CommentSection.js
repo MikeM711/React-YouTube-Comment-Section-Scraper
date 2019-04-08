8ca51d020b96e4f5f3d889a1736923d69e68f154
Michael <michaelmccabe711@gmail.com>
2019-04-08 16:17:30 -0400

    initializing the start of filters

--- client/src/components/CommentSection/CommentSection.js
+++ client/src/components/CommentSection/CommentSection.js
@@ -3,13 +3,86 @@ import ReplyComponent from  '../Reply/Reply'
 import './CommentSection.css'
 import undefAvatar from '../../Images/question-mark.jpg'
 import checkmarkImg from '../../Images/checkmark.png'
-// const JSONresult = require('../ResultTest')
+let JSONresult = require('../ResultTest2')
 
 class CommentSection extends Component {
 
   render() {
-    const { Result } = this.props
-    const JSONresult = JSON.parse(Result)
+    // const { Result } = this.props
+    // const JSONresult = JSON.parse(Result)
+
+    function CreatorReplyFilter(OPpost) {
+      if (OPpost.replies) {
+        var creatorReply = OPpost.replies.filter(replyPost => {
+          return replyPost.isCreatorRep === true
+        })
+        if(creatorReply.length > 0){
+          return OPpost
+        }
+      } 
+      return null
+    }
+
+    function LikesReplyFilter(OPpost,likeBound){
+      if (OPpost.replies) {
+        var likesFilter = OPpost.replies.filter(replyPost => {
+          let numRepLikes = Number(replyPost.likesRep)
+          return numRepLikes >= likeBound
+        })
+
+        if (likesFilter.length > 0) {
+          return OPpost
+        } else {
+          return null
+        }
+      }
+    }
+
+    function StringReplyFilter(OPpost) {
+      if (OPpost.replies) {
+        var stringReply = OPpost.replies.filter(replyPost => {
+          return replyPost.reply.toLowerCase().includes('night')
+        })
+        if(stringReply.length > 0){
+          return OPpost
+        }
+      } 
+      return null
+    }
+
+    // Filtering example
+    if(JSONresult){
+      JSONresult = JSONresult.filter(OPpost => {
+        
+        // return threads where the video creator is the OP
+        let creatorOPFilter = OPpost.isCreator === true
+        
+        // return threads where the video creator is the replier
+        let creatorRepFilter = CreatorReplyFilter(OPpost)
+
+        // return threads where the OP includes a certain string
+        let wordOPFilter = OPpost.comment.toLowerCase().includes('night')
+
+        // return threads where the replier includes a certain string
+        let stringRepFilter = StringReplyFilter(OPpost);
+        
+        // return threads that have one or more replies of a certain amount of likes
+        let likesRepFilter = LikesReplyFilter(OPpost,3)
+        
+        /*
+        Returns video creator threads AND video creator replies
+          return creatorOPFilter || creatorRepFilter
+
+        Returns full threads if OP posts and replies use a certain word
+          return stringRepFilter || wordOPFilter
+        */
+        
+        return OPpost
+
+        // return OPpost
+      })
+    }
+    
 
     const comments = JSONresult.length ? ( 
       JSONresult.map(OPcomment => {
@@ -17,6 +90,11 @@ class CommentSection extends Component {
         const Creator = OPcomment.isCreator ? ("comment-header-creator") : ("comment-header")
         const checkmark = OPcomment.isCreator ? (checkmarkImg) : (null)
         const Avatar = OPcomment.avatar !== "" ? (OPcomment.avatar) : (undefAvatar)
+
+        // Below variable adds "finder" to the className of a div to show what post has been found, due to filtering
+        
+        // const finder = " finder"
+        const finder = ""
         
         const replies = OPcomment.replies.length ? (
           OPcomment.replies.map(reply => {
@@ -45,7 +123,7 @@ class CommentSection extends Component {
                 {/*<span>Creator? {Creator} </span> */}
               </div>
 
-              <div className="comment-content">
+              <div className={"comment-content" + finder}>
                 <p>{OPcomment.comment}</p>
               </div>
 
@@ -64,6 +142,13 @@ class CommentSection extends Component {
         <div>
           <h2>Result</h2>
         </div>
+        {/* Filter component 
+          - Have a "filter state"
+          - send "filter" instructions by function out of component
+          - start with a hard-coded filter
+            - ie: comments/replies of isCreator and isCreatorRep = true
+        
+        */}
         <div className="comment-class">
           {comments}
         </div>
